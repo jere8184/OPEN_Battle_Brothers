8925ff8db2739d874b626aa562d05d0c992939c3
Jeremiah Morgan <JeremiahMorgan.bham@outlook.com>
2024-11-30 19:26:40 +0000

    inventory uses optional

--- Simulation/inventory.hpp
+++ Simulation/inventory.hpp
@@ -3,71 +3,60 @@
 #include <vector>
 #include <array>
 #include <unordered_map>
+#include <iostream>
+#include <optional>
 
-#include "item.hpp"
 
 
 
-//current implimnetation only works with pointer types
-template<typename Element_Type = Item*, std::size_t slots = 30>
+
+
+template<typename Storage_Type, std::size_t slots = 30>
 class Inventory
 {
 private:
+    using element_t = std::optional<Storage_Type>;
+    using array_t = std::array<element_t, slots>;
 
-    std::array<Element_Type, slots> inventory = {};
-    
-    using Slot = int;
-    std::unordered_map<Element_Type, Slot> unique_items;
+    array_t m_inventory = {};
+    //std::unordered_set<int> m_unique_ids;
 
-
-    std::array<Element_Type, slots>::iterator begin()
+    array_t::iterator begin()
     {
-        return inventory.begin();
+        return m_inventory.begin();
     }
 
-    std::array<Element_Type, slots>::iterator end()
+    array_t::iterator end()
     {
-        return inventory.end();
+        return m_inventory.end();
     }
 
 public:
     
     Inventory() = default;
 
-    template<typename T>
-    Inventory(const T& elements)
-    {
-        if(elements.size() > slots)
+    template<typename Iter>
+    Inventory(const Iter& start, const Iter& end)
+    {        
+        int s = 0;
+        for(auto& i = start; i != end ; i++)
         {
-            //throw error
-        }
-        
-        Slot s = 0;
-        for(Element_Type element :  elements)
-        {
-            if(unique_items.insert({element, s}).second)
-                inventory[s++] = element; 
+            if(s < m_inventory.size() == false)
+            {
+                break;
+            }
+
+            m_inventory[s++] = *i; 
         }
     };
 
 
    //returns -1 if no empty slot found, else returns next empty slot
-    Slot next_empty(Slot start = 0)
+    int next_empty(int start = 0)
     {
         for(int i = start; i < this->inventory.size(); i++)
         {
             if(!this->inventory[i])
-                return i;
-        }
-        return -1;
-    }
-
-    //returns -1 if no occupied slot found, else returns next occupied slot
-    Slot next_occupied(Slot start = 0)
-    {
-        for(int i = start; i < this->inventory.size(); i++)
-        {
-            if(this->inventory[i])
             {
                 return i;
             }
@@ -75,42 +64,48 @@ public:
         return -1;
     }
 
-    //place element at postion, postion, returns pointer to item that was already present at that postion or nullptr  
-    Element_Type place(Element_Type new_element, Slot s)
+    //returns -1 if no occupied slot found, else returns next occupied slot
+    int next_occupied(int start = 0)
     {
-        if(this->unique_items.insert({new_element, s}).second)
+        for(int i = start; i < this->m_inventory.size(); i++)
         {
-            Element_Type old_element = remove(s);
-            this->inventory[s] = new_element;
-            return old_element;
+            if(this->m_inventory[i])
+            {
+                return i;
+            }
         }
+        return -1;
+    }
 
-        //item already present throw
-        return nullptr;
+    //place element at postion, returns object that was already present at that postion or   
+    element_t place(Storage_Type& new_obj, int s)
+    {
+        element_t old_obj = remove(s);
+        this->m_inventory[s] = new_obj;
+        return old_obj;
     }
 
 
     //remove element from inventory at specified slot and return it
-    Element_Type remove(Slot s)
+    element_t remove(int s)
     {
-        Element_Type element = this->inventory[s];
-        this->inventory[s] = nullptr;
-        this->unique_items.erase(element);
-        return element;
+        element_t obj = this->m_inventory[s];
+        this->m_inventory[s].reset();
+        return obj;
     }
 
 
     //place all elements in other into empty slots of this inventory. Return true if all elements transfered else return false;
     template<size_t size>
-    bool Transfer(Inventory<Element_Type, size>& other)
+    bool Transfer(Inventory<Storage_Type, size>& other)
     {
-        Slot this_slot = 0;
-        Slot other_slot = 0;
+        int this_slot = 0;
+        int other_slot = 0;
         while(this_slot != -1 || other_slot != -1)
         {
             this_slot = this->next_empty(this_slot);
             other_slot = this->next_occupied(other_slot);
-            Element_Type removed_element = other->remove(other_slot);
+            element_t removed_element = other->remove(other_slot);
             this->place(removed_element);
         }
 
--- Simulation/tests/test_inventory.cpp
+++ Simulation/tests/test_inventory.cpp
@@ -6,8 +6,8 @@
 int main()
 {
     Inventory<int*, 30> inv;
-    int a = 4;
-    inv.place(&a, 5);
-    inv.remove(4);
+    int* a;
+    inv.place(a, 5);
+    inv.remove(5);
     std::cout << inv.next_occupied(0);
 };
